14706ab64a7aa07773f1bcdf6472f6a86c2c32c3
Andrei Cozma <andreig992@gmail.com>
2018-04-10 22:19:23 -0500

    a

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext('2d');
 
-
-
-
-
 var sides = prompt("How many sides? (Default 5)");
 var rad = prompt("What's the radius? (Default 50)")
 
@@ -27,12 +23,4 @@ function resize(){
     ctx.lineTo(canvas.width/2+ rad*Math.cos(((i+(360/sides))*(Math.PI/180))), canvas.height/2+rad*Math.sin(((i+360/sides)*(Math.PI/180))));
     ctx.stroke();
   }
-}
-
-
-function drawDot(x,y,rad){
-  ctx.beginPath();
-  ctx.arc(x,y,rad,0,Math.PI*2)
-  ctx.stroke();
-  ctx.fill();
 }
